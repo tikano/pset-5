fc1e1b99fcc5cb987a1e9b30f57180384c01b931
tikano <oadmathgenie@gmail.com>
2019-12-12 11:45:08 -0500

    2 exercise

--- js/shapes.js
+++ js/shapes.js
@@ -21,15 +21,29 @@ window.onload = function() {
     document.getElementById("hello").onclick = sayHello;
 }
 
+const start = function() {
+    const canvas = document.getElementById('student-canvas-2');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 /*
  * Exercise 1.
  */
 
 const sayHello = function() {
-   const canvas = document.getElementById('student-canvas-1');
-   const ctx = canvas.getContext('2d'); 
-   ctx.fillStyle = 'green';
-   ctx.fillRect(10, 10, 150, 100);
+   start();
+   ctx.font = "48px sans-serif";
+   
+   do {
+    var prompt = prompt("Message: ")
+    if (prompt.length > 50) {
+      alert("Your message is too long. Keep it under 50 characters")
+    }
+  } while(prompt.length > 50 || prompt == null)
+      
+  ctx.strokeText(message, 30, 70, 994);
+  
 };
 
 /*
@@ -37,7 +51,38 @@ const sayHello = function() {
  */
 
 const drawRectangle = function() {
-    // write your exercise 2 code here
+    start();
+    do {
+    var width = prompt("Width: ")
+    var height = prompt("Height: ")
+    var x = prompt("X: ")
+    var y = prompt("Y: ")
+    if (width == null || height == null || x == null || y == null) {
+      
+    }
+    if (width > 1024 || width < 1) {
+      alert("Your width must be between 1 and 1024.")
+    }
+    else if (height > 512 || height < 1) {
+      alert("Your height must be between 1 and 512.")
+    }
+    else if (x < 1 || x > 1024) {
+      alert("Your x-coordinate must be between 1 and 1024.")
+    }
+    else if (y < 1 || y > 512) {
+      alert("Your y-coordinate must be between 1 and 512.")
+    }
+    else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+      alert("One of your values is not a number.")
+    }
+    else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {
+      alert("Your rectangle won't fit on the canvas.")
+    }
+  } while (width > 1024 || width < 1 || height > 512 || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512 || width == null || height == null || x == null || y == null)
+  ctx.beginPath();
+  ctx.rect(x, y, width, height);
+  ctx.closePath();
+  ctx.stroke();    
 };
 
 /*
